d4a3d8c898206952f4034f605866d9fa6e1c69e0
zhouxi <zhouxi121@live.com>
2017-12-27 21:10:50 +0800

    Check

--- app/src/main/java/com/acgmiao/dev/fuckrunning/activity/MapsActivity.java
+++ app/src/main/java/com/acgmiao/dev/fuckrunning/activity/MapsActivity.java
@@ -8,56 +8,215 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.acgmiao.dev.fuckrunning.R;
 
+import java.util.List;
+
 public class MapsActivity extends AppCompatActivity implements
         OnMapReadyCallback,
         GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        LocationListener {
+
+    /**
+     * refresh Once per Second
+     */
+    private static final long LOCATION_REFRESH_TIME_INTERVAL = 1000;
+
+    /**
+     * min Meter
+     */
+    private static final float LOCATION_MIN_DESTANCE = 1.0f;
 
     private GoogleMap mMap;
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private boolean mPermissionDenied;
+
+    private LocationManager mLocationManager;
+
+    private boolean mIsAddListener;
+
+    /**
+     * is Permission Granted
+     */
+    private boolean mIsPermissionGranted;
+
+    private BroadcastReceiver broadcastReceiver;
+
     private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if(intent.getAction().equalsIgnoreCase(LocationManager.PROVIDERS_CHANGED_ACTION)){
+                    if(mIsPermissionGranted && mIsResume){
+                        boolean isGPSOn = isGPSOn();
+                        if(isGPSOn){
+                            startGetLocation();
+                        }else{
+                            stopGetLocation();
+                        }
+                    }
+                }
+            }
+        };
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private boolean mIsResume;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIsResume = true;
+        registerBroadcastReceiver();
+        if(mIsPermissionGranted){
+            startGetLocation();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mIsResume = true;
+        unregisterBroadcastReceiver();
+        if(mIsPermissionGranted){
+            stopGetLocation();
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        getLocationPermission();
-        updateLocationUI();
+        boolean isGotLocationPermission = checkLocationPermission();
+        if(isGotLocationPermission){
+            // goto next method
+            mIsPermissionGranted = true;
+            startGetLocation();
+        }else{
+            // need to get location permission
+            requestLocationPermission();
+        }
+
+
+//        updateLocationUI();
 
         //丢失默认事件，待修复
         //mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
+//        mMap.setOnMyLocationClickListener(this);
     }
 
-    public void getLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            mPermissionDenied = true;
-            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE, android.Manifest.permission.ACCESS_FINE_LOCATION, false);
-        } else {
-            mPermissionDenied = false;
+    private void registerBroadcastReceiver(){
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LocationManager.PROVIDERS_CHANGED_ACTION);
+        registerReceiver(broadcastReceiver,intentFilter);
+    }
+
+    private void unregisterBroadcastReceiver(){
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    private void initLocationManager(){
+        if(mLocationManager == null){
+            mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         }
+    }
+
+    private void startGetLocation(){
+        initLocationManager();
+        if(!mIsAddListener){
+            List<String> providers = mLocationManager.getProviders(true);
+            for(int i = 0 ; i < providers.size() ; i ++ ) {
+                String provider = providers.get(i);
+                Log.e("test","startGetLocation:"+provider);
+                mLocationManager.requestLocationUpdates(
+                        provider,LOCATION_REFRESH_TIME_INTERVAL,LOCATION_MIN_DESTANCE,this);
+            }
+            mIsAddListener = true;
+        }
+    }
+
+    private void stopGetLocation(){
+        initLocationManager();
+        if(mIsAddListener){
+            Log.e("test","stopGetLocation");
+            mLocationManager.removeUpdates(this);
+            mIsAddListener = false;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.e("test","onLocationChanged:"+location.toString()+",Provider:"+location.getProvider());
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.e("test","onStatusChanged:"+s+","+i);
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.e("test","onProviderEnabled:"+s);
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.e("test","onProviderDisabled:"+s);
+
+    }
+
+    private boolean isGPSOn(){
+        initLocationManager();
+        boolean gpsOn = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return gpsOn;
+    }
+
+    /**
+     * check Location Permission
+     * @return
+     */
+    private boolean checkLocationPermission(){
+        boolean locationPermission = false;
+        int permissionStatus = ContextCompat.
+                checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionStatus == PackageManager.PERMISSION_GRANTED){
+            locationPermission = true;
+        }
+        return locationPermission;
+    }
+
+    /**
+     * requestLocationPermission
+     */
+    public void requestLocationPermission() {
+        PermissionUtils.requestPermission(this,
+                PermissionUtils.LOCATION_PERMISSION_REQUEST_CODE,
+                android.Manifest.permission.ACCESS_FINE_LOCATION, false);
     }
 
 
@@ -65,18 +224,18 @@ public class MapsActivity extends AppCompatActivity implements
         if (mMap == null) {
             return;
         }
-        try {
-            if (mPermissionDenied) {
-                mMap.setMyLocationEnabled(false);
-                mMap.getUiSettings().setMyLocationButtonEnabled(false);
-            } else {
-                mMap.setMyLocationEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-                mMap.setLocationSource(new MyLocationSource());
-            }
-        } catch (SecurityException e) {
-            Log.e("Exception: %s", e.getMessage());
-        }
+//        try {
+//            if (mPermissionDenied) {
+//                mMap.setMyLocationEnabled(false);
+//                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+//            } else {
+//                mMap.setMyLocationEnabled(true);
+//                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+//                mMap.setLocationSource(new MyLocationSource());
+//            }
+//        } catch (SecurityException e) {
+//            Log.e("Exception: %s", e.getMessage());
+//        }
     }
 
     @Override
@@ -95,26 +254,28 @@ public class MapsActivity extends AppCompatActivity implements
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
-        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+
+        if (requestCode != PermissionUtils.LOCATION_PERMISSION_REQUEST_CODE) {
             return;
         }
         if (PermissionUtils.isPermissionGranted(permissions, grantResults,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
-            mPermissionDenied = false;
-            updateLocationUI();
+            mIsPermissionGranted = true;
+            initLocationManager();
+            startGetLocation();
         } else {
-            mPermissionDenied = true;
-            updateLocationUI();
+            mIsPermissionGranted = false;
+            Log.e("test","not get location permission");
         }
     }
 
     @Override
     protected void onResumeFragments() {
         super.onResumeFragments();
-        if (mPermissionDenied) {
-            showMissingPermissionError();
-            mPermissionDenied = false;
-        }
+//        if (mPermissionDenied) {
+//            showMissingPermissionError();
+//            mPermissionDenied = false;
+//        }
     }
 
     private void showMissingPermissionError() {
@@ -123,19 +284,21 @@ public class MapsActivity extends AppCompatActivity implements
 
     private LocationSource.OnLocationChangedListener myLocationListener = null;
 
+
+
     private class MyLocationSource implements LocationSource {
         @Override
         public void activate(OnLocationChangedListener listener) {
-            myLocationListener = listener;
-            MyLocation mylocation = new MyLocation();
-            mylocation.getLocation();
-            if (mylocation.location == null) {
-                //什么乱七八糟的
-                mPermissionDenied = true;
-                updateLocationUI();
-            } else {
-                myLocationListener.onLocationChanged(mylocation.location);
-            }
+//            myLocationListener = listener;
+//            MyLocation mylocation = new MyLocation();
+//            mylocation.getLocation();
+//            if (mylocation.location == null) {
+//                //什么乱七八糟的
+//                mPermissionDenied = true;
+//                updateLocationUI();
+//            } else {
+//                myLocationListener.onLocationChanged(mylocation.location);
+//            }
         }
 
         @Override
